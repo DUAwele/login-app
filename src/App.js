5399535912ec1a9c5e98ad8e522019ef22683f85
DUAwele <zthunderzthunder@gmail.com>
2025-05-02 00:55:52 -0500

    made a register page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
+import Layout from './layout';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
+  return (
+    <Router>
+    <Layout>
+        <Routes>
+           <Route path="/" element   />
 
+           <Route path="/add" element    />
+        </Routes>
+    </Layout>
+</Router>
+
+)
 }
 
 export default App;
--- src/layout.js
+++ src/layout.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./layout.css"
 
 function Layout({children}) 
 {
     return (
          <div className="container mt-4">
-            <h3 className="text-center mb-4 bg-danger bg-gradient fst-italic text-light">DAV CAR WASH</h3>
+            <h3 className="text-center mb-4 bg-danger bg-gradient fst-italic text-light">Mechanic quiz</h3>
             <nav>
-                <Link className="btn btn-warning me-2 fst-italic reg" to="/"> Home </Link>
+                <Link className="btn btn-warning me-2 fst-italic reg" to="/"> Register </Link>
                 <Link className="btn btn-warning me-2 fst-italic reg" to="/add"> Login </Link>
             </nav>
             {children}
