b2d5aace3507df6305f5fc2ac5c433db0237a5a4
DUAwele <zthunderzthunder@gmail.com>
2025-05-04 13:28:18 -0500

    new category

--- src/layout.js
+++ src/layout.js
@@ -6,7 +6,7 @@ import "./layout.css"
 function Layout({children}) 
 {
     return (
-         <div className="container mt-4 well">
+         <div className="container mt-4 bg-danger">
             <h3 className="text-center mb-4 bg-dark bg-gradient fst-italic text-light">Sign up and log in</h3>
             <nav className='bg-gradient d-flex justify-content-center align-items-center' >
                 <Link className="btn btn-light btn btn-outline-dark me-2 fst-italic reg" to="/"> Register </Link>
